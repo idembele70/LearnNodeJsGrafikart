b82b636b155bc1c2a4823242dc5f293ab83d0bad
idembele70 <idembele70@gmail.com>
2021-03-29 19:33:39 +0200

    premier server first 20min

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 let http = require('http');
-http.createServer()
+let server = http.createServer();
+
+server.on('request', function(request, response) {
+    response.writeHead(200, {
+        'Content-type': 'text/html; charset=utf-8'
+    });
+
+    response.end('Salut comment ça va ?');
+})
+
+server.listen(8080);
